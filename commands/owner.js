c341315f4b6d59388ba79c3ec4ca2daa186eb72c
Spxrsh <127536158+spxrsh-04@users.noreply.github.com>
2024-02-11 17:04:58 +0545

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -24,17 +24,15 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/FUEHs7RCqz';
-      const InstagramLink = 'https://www.instagram.com/rtxxgg/';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
-            .setColor('#da2a41')
+            .setColor('#000000')
             .setAuthor({
           name: 'Owner',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/bharat'
         })
-            .setDescription(`__**About me**__:\n\n ▶️ Myself Shiva aka RTX. I am a discord bot developer and web developer. I love playing games, watching anime and building different webserver applications. You will get faster replies on instagram than other social media. Feel free to contact me!\n YouTube : ❤️ [RTX GAMING](${youtubeLink})\n Instagram : 💙 [rtxxgg](${InstagramLink})`)
+            .setDescription(`__**About me**__:\n\n ▶️ Spxrsh here what you lookin at listen to songs mate. })`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
